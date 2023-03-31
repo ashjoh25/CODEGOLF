50c8fb48d55248469e242dfc938bd64d7e9cf219
ashjoh25 <98178302+ashjoh25@users.noreply.github.com>
2023-03-31 19:29:49 -0400

    Add files via upload

---
+++ fizz.java
@@ -0,0 +1,23 @@
+package CODEGOLF;
+public class fizz {
+    public static void main(String[] args) {
+        buzz();
+    }
+
+    public static void buzz() {
+        for (int i = 1; i < 101; i++) {
+            if (i % 3 == 0 && i % 5 == 0 ) {
+                System.out.println("FizzBuzz");
+            }
+            else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
+    }
+}
